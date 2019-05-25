4cb93f3401c16671f40935b90b9b0d4b49117947
janalexandersson <janalexandersson97@gmail.com>
2019-05-25 19:01:42 +0200

    Added the graph representation and the reading of file

--- DataToGraphConverter.java
+++ DataToGraphConverter.java
@@ -1,11 +1,104 @@
 //JAN will look at this during the weekend
+import java.io.*;
+import java.util.*;
+
+
+/*
+0. Identifier for one contig in overlap.
+1. Identifier for second contig in overlap.
+2. A umber representing similarity, but not relevant for this project.
+3. Fraction of identical positions in the overlap.
+4. A zero. Unused.
+5. Start of overlap in first contig.
+6. End of overlap in first contig.
+7. Length of first contig.
+8. A zero (0) or one (1) to say whether the overlap is on the reverse sequence (1) or not (0). See below for how this is used.
+9. Start of overlap in second contig.
+10. End of overlap in second contig.
+11. Length of second contig.
+*/
+
 
 public class DataToGraphConverter{
 	
-	public DataToGraphConverter(){
-
+	
+	private DataToGraphConverter(){
+		
 	}
-
-	//JAN will look at this during the weekend
+	
+	
+	public static Graph readM4(String filename) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(filename));
+		return readM4(br);
+	}
+	
+	public static Graph readM4(BufferedReader br) throws IOException{
+		
+		String line;
+		line = br.readLine();
+		
+		Graph g = new Graph(0);
+		
+		int lineNumber = 0;
+		
+		//Note that every node we read is connected with at least one edge, according to data
+		while (( line = br.readLine()) != null ){
+		
+		
+		//Investigating the memory usage after the GC error ( garbage collection)
+			lineNumber++;
+			if(lineNumber %100000 == 0){
+				long freeMem = Runtime.getRuntime().freeMemory();
+				long maxMem = Runtime.getRuntime().maxMemory();
+				double usage = (100.0*freeMem)/maxMem;
+				System.out.println(lineNumber + " \t" + usage + "\t  Max:" + maxMem/(1024.0*1024));
+			}
+			
+			String[] lineEntries = line.split("\\s+");
+			
+			String nodeA = lineEntries[0];
+			String nodeB = lineEntries[1];
+			
+			
+			int startA = Integer.parseInt(lineEntries[5]);
+			int endA = Integer.parseInt(lineEntries[6]);
+			int lengthA = Integer.parseInt(lineEntries[7]);
+			
+			int startB = Integer.parseInt(lineEntries[5]);
+			int endB = Integer.parseInt(lineEntries[6]);
+			int lengthB = Integer.parseInt(lineEntries[7]);
+			
+			//Make sure the overlap is not substring
+			//Sometimes, the overlap is actually containment: if contig A is a subsequence of contig B, then A can be discarded.
+			if(endA - startA < lengthA && endB - startB < lengthB ){
+				g.addEdge(nodeA, nodeB);
+			}
+			
+		} 
+		
+		return g;
+	}
+	
+	
+	//Do run this you need to allocate 8G
+	//Run this: java -Xmx8G DataToGraphConverter ../overlaps.m4
+	public static void main(String[] args){
+		String filename = args[0];
+		
+		Graph g = null;
+		
+		try{
+			g = DataToGraphConverter.readM4(filename);
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+		
+		g.connectedComponents();
+		
+		g.removeNode(5);
+	}
+	
+	
 	
 }
--- Graph.java
+++ Graph.java
@@ -2,57 +2,150 @@
 //adjacency matrix takes more space but is more efficient with removing edges etc
 //adjacency list takes less space but can be inefficient at finding of there is and edge between two nodes
 
+import java.io.*;
+import java.util.*;
 
-
-//JAN will look at this during the weekend
+// ajdList represents the graph
+//nodeNameToIndex translates between nodename to index
+//indexToNodeName translates between index to nodename
 public class Graph{
 
 
-	int n;
 	//We want to store the vertices as integers to save space
 	//Need some way to translate between integers and vertex names
-	LinkedList<Integer> adjList[];
+	ArrayList<Collection<Integer>> adjList;
+	HashMap<String, Integer> nodeNameToIndex = new HashMap<String, Integer>();
+	HashMap<Integer, String> indexToNodeName = new HashMap<Integer, String>();
 	
 	public Graph(int n){
-		this.n = n;
 				
-		adjList = new LinkedList[n]; 
+		adjList = new ArrayList(n); 
               
 		// Create a new list for each vertex 
 		// adjacent vertices will be stored in the list
 		for(int i = 0; i < n ; i++){ 
-			adjList[i] = new LinkedList<>(); 
+			adjList.add(new ArrayList<Integer>()); 
 		} 
 	}
-
 	
-	public void addEdge(int from, int to) { 
-        // Add an edge from 'from' to 'to'.  
-        this.adjList[from].add(from); 
+	//It might be better to use TreeSet instead of ArrayList for storing the neightbors
+	//Because then it will be faster to remove specified edges from our adjacency list
+	public void addEdge(int from, int to) {  
+		//Add more vertices with no edges
+        while( adjList.size() <= Math.max(from, to)){
+			adjList.add(new ArrayList<Integer>());
+        }
+        
+		// Add an edge from 'from' to 'to'. 
+        this.adjList.get(from).add(to); 
           
 		// Add that edge to the other vertex too because our graph is undirected
-        this.adjList[to].add(to); 
+        this.adjList.get(to).add(from); 
     } 
 	
-	
-	public void removeEdge(int from, int to) { 
+	public void addEdge(String from, String to) {  
+		if(!nodeNameToIndex.containsKey(from)){
+			int i =  nodeNameToIndex.size();
+			nodeNameToIndex.put(from, i);
+			indexToNodeName.put(i, from);
+		}
 		
-		//JAN will look at this during the weekend
+		if(!nodeNameToIndex.containsKey(to)){
+			int i =  nodeNameToIndex.size();
+			nodeNameToIndex.put(to, i);
+			indexToNodeName.put(i, to);
+		}
 		
+		//Map from string to int, stringname to index
+		int fromIndex = nodeNameToIndex.get(from);
+		int toIndex = nodeNameToIndex.get(to);
+		
+		addEdge(fromIndex, toIndex);
     } 
 	
 	
-	public void removeNode(int node) { 
-		//JAN will look at this during the weekend
+	public void printAllEdges() { 
+		
+		for(int v = 0; v < adjList.size(); v++){
+			Collection<Integer> vNeighbors = adjList.get(v);
+			
+			
+			for(int n: vNeighbors){
+				System.out.println(v + " " + n);
+			}
+			
+			
+// 			for(int n = 0; n< vNeighbors.size(); n++){
+// 				System.out.println(v + " " + vNeighbors.get(n));
+// 			}
+		}
     } 
 	
 	
+	public String indexToNodeName(int index){
+		return indexToNodeName.get(index);
+	}
 	
 	
-	//Some algorithm for counting partitions will be implemented below etc..
+	//Fill in the connected component containing node "startNode" with the number "componentNumber"
+	//This uses Flood Fill
+	private void floodFillHelper(int[] indicator, int startNode, int componentNumber){
+		
+		Stack<Integer> toVisit = new Stack<Integer>();
+		
+		indicator[startNode] = componentNumber;
+		toVisit.push(startNode);
+		
+		//recursion did not work due to stack overflow
+		while(!toVisit.isEmpty()){
+			int node = toVisit.pop();
+			indicator[node] = componentNumber;
+			
+			
+			//loop over neighbors and push nodes to visit
+			for(int n: adjList.get(node)){
+				if(indicator[n] != componentNumber){
+					toVisit.push(n);
+				}
+			}
+		}
+				
+	}
+	
+	
+	//If there are components of size 1, those should be discarded beacuase we technically removed the node (leaved it as isolated)
+	public int[] connectedComponents(){
+		int[] indicator = new int[adjList.size()];
+		
+		int k = 1;
+		for(int i = 0; i<indicator.length; i++){
+			//if node i does not belong to a component yet, it has value 0
+			if(indicator[i] == 0){
+				//paint component with color k
+				floodFillHelper(indicator, i, k);
+				k++;
+			}
+		}
+		System.out.println(k);
+		return indicator;
+	}
 	
 	
 	
+	//This will leave nodes as isolated, we know that we did not have any isolated nodes in our base graph
+	public void removeNode(int nodeToRemove) { 
+		
+		Collection<Integer> neightbors = adjList.get(nodeToRemove);
+		
+		for(int n: neightbors){
+			adjList.get(n).remove(nodeToRemove);
+		}
+		
+		adjList.get(nodeToRemove).clear();
+    } 
+    //To count number of removed edges we can just see how many edges we had before partitoning and then subrtact edges after partitoning
+	
+
 	
 	
 	
