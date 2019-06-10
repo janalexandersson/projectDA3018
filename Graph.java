05baf06f298327a0418b405aa2fe9b5f155cd174
Fredrika-Lundahl <44748577+Fredrika-Lundahl@users.noreply.github.com>
2019-06-10 07:29:59 +0200

    added a comment

--- Graph.java
+++ Graph.java
@@ -393,7 +393,7 @@ public class Graph{
     }
 
 	
-	//for statistics
+	//final partitions to new file
 	public void writePartitions(int[] componentSet){
 		try{
 			FileWriter fw=new FileWriter("partitions.txt");    
